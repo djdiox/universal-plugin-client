89f8d4623378f2499faf99a6e31974ac3952f41e
Markus <markus.wagner@zoi.de>
2022-06-10 12:21:18 +0200

    init

---
+++ src/renderer/pages/apps.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+        
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({})
+export default class Apps extends Vue {}
+</script>
+
+<style ${2|scoped,|} lang="scss">
+</style>
